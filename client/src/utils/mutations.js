72b87b1f696aff45f3445017653151433d475dad
albenchris <albenchris00@gmail.com>
2021-05-07 16:31:14 -0500

    front end SAVE_BOOK mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -23,3 +23,21 @@ export const ADD_USER = gql`
         }
     }
 `;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($input: savedBook!) {
+        saveBook(input $input) {
+            _id
+            username
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
