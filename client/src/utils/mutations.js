91a371343f0d5a83dd5a34fbecfa3ccfc5088fba
albenchris <albenchris00@gmail.com>
2021-05-07 17:12:35 -0500

    changed LoginForm component to use new LOGIN_USER mutation, also wrapped App in ApolloProvider to debug

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -26,7 +26,7 @@ export const ADD_USER = gql`
 
 export const SAVE_BOOK = gql`
     mutation saveBook($input: savedBook!) {
-        saveBook(input $input) {
+        saveBook(input: $input) {
             _id
             username
             bookCount
